5831007bd3870880d2fe51fe9fe97d7a012790d1
doncilafx <sergiu.doncila@microlab.utm.md>
2023-11-29 20:58:18 +0200

    change connection to pooling

--- mqtt.js-client-example/db.js
+++ mqtt.js-client-example/db.js
@@ -1,21 +1,39 @@
 import mysql from "mysql2"
 import dbConfig from "./db.config.js";
 
+//var connectionState = false;
+
 // Create a connection to the database
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+//   port: dbConfig.PORT
+// });
+
+const pool = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
-  port: dbConfig.PORT
+  port: dbConfig.PORT,
+  connectionLimit: 10
 });
 
 // open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  //if (error) console.log("Error");
-  else console.log("Successfully connected to the database.");
-});
+// connection.connect(error => {
+//   if (error) throw error;
+//   //if (error) console.log("Error");
+//   else console.log("Successfully connected to the database.");
+// });
+
+// pool.getConnection(function(err, connection) {
+//   if (err) throw err;
+ 
+//   connection.release();
+// });
 
 //module.exports = connection;
-export default connection;
+//export default connection;
+export default pool;
--- node-api/src/models/db.js
+++ node-api/src/models/db.js
@@ -1,21 +1,32 @@
 import mysql from "mysql2"
 import dbConfig from "../mysql/db.config.js";
 
-// Create a connection to the database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
-  port: dbConfig.PORT
+  port: dbConfig.PORT,
+  connectionLimit: 10
 });
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  //if (error) console.log("Error");
-  else console.log("Successfully connected to the database.");
-});
+
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+//   port: dbConfig.PORT
+// });
+
+// // open the MySQL connection
+// connection.connect(error => {
+//   if (error) throw error;
+//   //if (error) console.log("Error");
+//   else console.log("Successfully connected to the database.");
+// });
 
 //module.exports = connection;
-export default connection;
+//export default connection;
+export default pool;
