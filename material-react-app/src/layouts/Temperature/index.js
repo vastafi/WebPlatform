4512ab8c94b72339c9ffe29c01d7826a999051d8
Astafi Valentina <valentina.astafi@isa.utm.md>
2024-01-22 17:35:13 +0200

    edit temperature page

--- material-react-app/src/layouts/Temperature/index.js
+++ material-react-app/src/layouts/Temperature/index.js
@@ -1,42 +1,53 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
 import MDBox from "components/MDBox";
 import Footer from "examples/Footer";
+import { useEffect, useState } from "react";
+
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 
 import Grid from "@mui/material/Grid";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
-import MDInput from "components/MDInput";
-import { Bar } from 'react-chartjs-2';
-import { Box } from "@mui/material";
-import { Chart as ChartJS, plugins } from "chart.js/auto"
 
-import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import MDButton from "components/MDButton";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 
 
+import MDTypography from "components/MDTypography";
 import mqtt from "mqtt";
 import { host } from "../../config/mqtt.config";
-import MDTypography from "components/MDTypography";
-import moment from 'moment';
-import {options, data} from "./temp"
+import { options } from "./temp";
+
+function createData(
+  name,
+  t1,
+) {
+  return { name, t1};
+}
+
+const rows = [
+  createData('36,5 °C – 37,4 °C','Normal temperature'),
+  createData('37,5 °C – 38,0 °C', 'Subfebrile Temperature'),
+  createData('38,1 °C – 38,5 °C', 'Mild fever'),
+  createData('38,6 °C – 39,0 °C', 'Moderate fever'),
+  createData('39,1 °C – 39,9 °C', 'High fever'),
+  createData('40,0 °C – 42,0 °C', 'Very high fever'),
+];
+
 
 const Temperature = () => {
 
   const [messages, setMessages] = useState({});
   const [count, setCount] = useState(4);
-  const [tempData, setTempData] = useState({
-    labels: ["Luni", "Marti", "Miercuri", "Joi", "Vineri", "Simbata", "Duminica"],
-    datasets: [{
-      label: "Temperature",
-      data: data,
-      pointBorderColor: "transparent",
-      pointStyle: false,
-      tension: 0.1
-    }]
-  })
+  
   console.log(messages);
 
   async function getMessages() {
@@ -48,24 +59,23 @@ const Temperature = () => {
       let result = response.data;
       console.log(result.length);
 
-      let shortResult = result.splice(result.length - 20, result.length);
-
-      const parsedLabels = shortResult.map(x => {
-        const seconds = moment(x.date).get('seconds');
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
-      });
+      let shortResult = result.splice(-50);
+      //let shortResult = result.splice(result.length - 20, result.length);
       setMessages({
-        labels: parsedLabels,
-        datasets: { label: "Temperature", data: shortResult.map(x => JSON.parse(x.message).temperature) },
-      });
+        labels: shortResult.map(x=> x.message_id),
+        datasets:
+            {
+                label: "temp_c",
+                data: shortResult.map(x => JSON.parse(x.message).temp_c),
+            }
+    });
 
-      const resp = shortResult.map(x => JSON.parse(x.message).temp);
-      setCount(resp[49] + "%");
-    } catch (error) {
-      console.error(error);
-    }
+    const resp = shortResult.map(x=> JSON.parse(x.message).temp_c);
+    setCount(resp[49] + "%");
+} catch (error) {
+    console.error(error);
+}
+
   }
 
   useEffect(() => {
@@ -81,36 +91,32 @@ const Temperature = () => {
   /*   Set Data */
   //const [settings, setSettings] = useState(null);
   const [settingsTempTime, setSettingsTempTime] = useState(0);
+  const [setPoint, setPointSettings] = useState(60);
 
   function setMqttData() {
     try {
-      let publishSettings = JSON.stringify({ 'tempTime': settingsTempTime });
-      console.log(publishSettings);
+      let publishSettings = JSON.stringify({ 'temp_c': settingsTempTime });
+            console.log(publishSettings);
 
-      let result = client.publish('microlab/agro/device/ventilation/settings', publishSettings);
+      let result = client.publish('hearth/device/smart_watch/temp/temp_c', publishSettings);
       console.log(result);
     } catch (error) {
       console.error(error);
     }
   }
 
-  const changeTempSettingsHandler = (e) => {
-    setSettingsTempTime(e.target.value);
-  };
-  // const changeHandler = (e) => {
-  //   setSettings(e.target.value);
+  // const changeTempSettingsHandler = (e) => {
+  //   setSettingsTempTime(e.target.value);
+  //   setPointSettings(e.target.value);
   // };
-
-
-
+  
   /* MQTT */
   const [client, setClient] = useState(null);
   const [connectStatus, setConnectStatus] = useState(null);
 
-  const [temp, setTemp] = useState(0);
+  const [temperature, setTemp] = useState(0);
 
   const tempTopic = 'hearth/device/smart_watch/temp';
-
 
   const mqttConnect = () => {
     setConnectStatus('Connecting');
@@ -129,6 +135,8 @@ const Temperature = () => {
         setConnectStatus('Connected');
 
         client.subscribe(tempTopic);
+
+
       });
       client.on('error', (err) => {
         console.error('Connection error: ', err);
@@ -148,8 +156,7 @@ const Temperature = () => {
     }
   }, [client]);
 
-
-  return (
+return (
     <DashboardLayout marginLeft={274}>
       <DashboardNavbar />
       <MDBox py={3}>
@@ -160,12 +167,12 @@ const Temperature = () => {
                 color="primary"
                 icon="thermostat"
                 title="Temperature"
-                count={temp}
-              // percentage={{
-              //   color: "success",
-              //   amount: "+1",
-              //   label: "difference",
-              // }}
+                count={count}
+                percentage={{
+                color: "success",
+                amount: "+1",
+                label: "difference",
+                }}
               />
             </MDBox>
           </Grid>
@@ -185,14 +192,7 @@ const Temperature = () => {
 
             <MDBox mb={1.5}>
               <MDBox mb={2}>
-                <MDInput
-                  type="text"
-                  label="Temp time (sec)"
-                  fullWidth
-                  value={settingsTempTime}
-                  name="settingsTempTime"
-                  onChange={changeTempSettingsHandler}
-                />
+               
               </MDBox>
 
             </MDBox>
@@ -204,8 +204,8 @@ const Temperature = () => {
               <MDTypography > MQTT status : {connectStatus}</MDTypography>
             </MDBox>
             <MDBox mb={1.5}>
-              <MDTypography > Current Settings :</MDTypography>
-              <MDTypography > Temperature time :{settingsTempTime}</MDTypography>
+              <MDTypography > Current Settings : </MDTypography>
+              <MDTypography > Temperature time : {settingsTempTime}</MDTypography>
             </MDBox>
 
           </Grid>
@@ -213,17 +213,58 @@ const Temperature = () => {
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
 
-              <Box width="100%">
-                  <div style={{height:"200px"}}>
-                    <Bar data={tempData} options={options} plugins={[options.plugin]}/>
-                  </div>
-              </Box>
-
+          <Grid item xs={12} md={12} lg={12}>
+                            <MDBox mb={3}>
+                                <ReportsLineChart
+                                    color="success"
+                                    title="Temperature Chart Celsius °C"
+                                    chart={messages}
+                                    date={"just updated"}
+                                />
+                            </MDBox>
+                        </Grid>
           </Grid>
         </MDBox>
 
+        <MDBox mt={4.5}>
+          <Grid container spacing={3}>
+
+          <Grid item xs={12} md={12} lg={12}>
+                            <MDBox mb={3}>
+                                <ReportsLineChart
+                                    color="success"
+                                    title="Temperature Chart Fahrenheit °F"
+                                    chart={messages}
+                                    date={"just updated"}
+                                />
+                            </MDBox>
+                        </Grid>
+          </Grid>
+        </MDBox>
       </MDBox>
-      <Footer />
+     <b> Body temperature: normal range and deviations</b>
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Temperature range</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{row.name} </TableCell>
+             <TableCell component="th" scope="row" >{row.t1}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </TableContainer>
+     <Footer />
     </DashboardLayout>
   );
 };
